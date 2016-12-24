356ceb82784bde2243732a8e82303a8e5a80d932
aravinderiventy <aravind.eriventy@gmail.com>
2016-12-24 20:29:38 +0530

    Aravind
    Added New logic

---
+++ URL_Routing/WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "MyView1",
+                url: "abc",
+                defaults: new { controller = "Test", action = "Jeevan", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "MyView",
+                url: "",
+                defaults: new { controller = "C2V", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ URL_Routing/WebApplication1/Controllers/C2VController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class C2VController : Controller
+    {
+        // GET: C2V
+        public ActionResult Index()
+        {
+            ViewData["mydate"] = DateTime.Now.ToString();
+            ViewData["slno"] = "123";
+            return View();
+        }
+    }
+}
